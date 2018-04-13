3a11a25bee38a551267dff6cd960ac9de9e7dadf
Adriaan Marain <adriaanmarain300@gmail.com>
2018-04-13 10:07:15 +0200

    minor fixes

--- ios/build/Build/Products/Debug-iphonesimulator/include/RNSound/RNSound.h
+++ ios/build/Build/Products/Debug-iphonesimulator/include/RNSound/RNSound.h
@@ -1,11 +1,12 @@
-#if __has_include("RCTBridgeModule.h")
-    #import "RCTBridgeModule.h"
-#else
+#if __has_include(<React/RCTBridgeModule.h>)
     #import <React/RCTBridgeModule.h>
+#else
+    #import "RCTBridgeModule.h"
 #endif
 
 #import <AVFoundation/AVFoundation.h>
+#import <React/RCTEventEmitter.h>
 
-@interface RNSound : NSObject <RCTBridgeModule, AVAudioPlayerDelegate>
-
+@interface RNSound : RCTEventEmitter <RCTBridgeModule, AVAudioPlayerDelegate>
+@property (nonatomic, weak) NSNumber* _key;
 @end
